958eeb0a5a47fbd75806d189c1ee81e55e29fe50
alegarz4 <mtra.claudiaalejandragarza@gmail.com>
2026-03-04 17:04:54 -0600

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
-const CACHE = "vinogastos-cache-v2";
+const CACHE = "vinogastos-cache-v3";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./apple-touch-icon.png"
+  "./icon-192-pro.png",
+  "./icon-512-pro.png",
+  "./apple-touch-icon-pro.png",
+  "./favicon-64-pro.png"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
@@ -25,14 +24,12 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  const req = event.request;
-
   event.respondWith(
-    caches.match(req).then((cached) => {
-      return cached || fetch(req).then((res) => {
-        if (req.method === "GET" && res.status === 200 && res.type === "basic") {
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((res) => {
+        if (event.request.method === "GET" && res.status === 200 && res.type === "basic") {
           const clone = res.clone();
-          caches.open(CACHE).then(cache => cache.put(req, clone));
+          caches.open(CACHE).then(cache => cache.put(event.request, clone));
         }
         return res;
       }).catch(() => cached);
